298c7b90b3a352eb74685bff4f1471a3c716c1f3
Alexander T.C <82457422+FluxFeint@users.noreply.github.com>
2023-04-24 17:39:04 +0000

    a

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,7 +9,7 @@ export const Home = () => (
 			<img src={rigoImage} />
 		</p>
 		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
+			If you see this green button, bootstrap is workingdasdsa
 		</a>
 	</div>
 );
